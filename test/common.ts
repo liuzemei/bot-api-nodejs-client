1f5068d538edb0b62563659ef860d657fed773d7
Neo <her_liu@qq.com>
2022-12-29 16:04:36 +0800

    abandoned

--- test/common.ts
+++
@@ -1,6 +0,0 @@
-import { Client } from '../src';
-import fs from 'fs';
-const keystore = JSON.parse(fs.readFileSync(__dirname + '/../config.json', 'utf8'));
-const client = new Client(keystore);
-
-export { client };
